131698a4ece22a31c486632f5e0353c4ff8d84e6
Brenda Wong <Brendot.wong@gmail.com>
2021-07-29 10:45:18 -0400

    fix: update on calculatePayout and displayWinner

--- client/classes/Game.js
+++ client/classes/Game.js
@@ -4,39 +4,19 @@ import ComputerPlayer from "./ComputerPlayer";
 
 export default class Game {
   constructor() {
-    //initiating new player, dealer, computer player here rather than in startGame
-
     this.player = new Player();
     //will have to change logic for a logged in user
 
     this.dealer = new Dealer();
     this.computerPlayer = new ComputerPlayer();
-    this.allPlayers = [this.player, this.computerPlayer];
-
-    //thinking rather than have active players in the constructor of the game, we can add isActive to player class and toggle between true and false (which is set when player busts)
-    // this.activePlayers = {};
+    this.allPlayers = [this.computerPlayer, this.player];
+    this.outcome = "";
   }
 
-  getActivePlayers() {
-    //   if we instead add isActive to the player class
-    //   if this.activePlayers is in the constructor:
-    //   return Object.keys(this.activePlayers).filter((player) => activePlayers[player]);
-  }
-
-  //this method is reliant on adding activePlayers to the constructor
-  //   removePlayer(player) {
-  //       this.activePlayers[player] = false;
-  //   }
-
-  startGame() {
-    //   "instantiates Player, AI Player(s), and Dealer if there are none already"
-    // ^^NOTE: wouldn't this happen when we want to create new Game anyway?
-    // NOTE: should the startGame() function be for starting ANOTHER game/start over rather than starting a brand-new game?
-
-    // the following instead might be better off in the Game's constructor
-    // new ComputerPlayer();
-    // new Player();
-    // new Dealer();
+  playAgain() {
+    this.player.updateCurrentCards([]);
+    this.computerPlayer.updateCurrentCards([]);
+    this.outcome = "";
     if (this.dealer.deck.length <= 26) {
       this.dealer.shuffle();
     }
@@ -45,41 +25,41 @@ export default class Game {
 
   endGame() {
     this.findWinner();
-    this.player.updateCurrentCards([]);
-    this.computerPlayer.updateCurrentCards([]);
-    //not necessary to call player.updateMoney again here as findWinner > calculatePayout > updateTotalMoney
   }
 
   findWinner() {
     let playerHand = this.player.getCardTotal();
     let dealerHand = this.computerPlayer.getCardTotal();
-    let winner;
-    let loser;
     if (playerHand == dealerHand) {
-      //draw; no need to calculate payout and can just return message
-      return "Draw";
+      this.outcome = "Tied";
     } else if (playerHand > dealerHand && playerHand <= 21) {
-      winner = this.player;
-      loser = this.computerPlayer;
+      this.outcome = "Win";
     } else if (playerHand < dealerHand && dealerHand <= 21) {
-      winner = this.computerPlayer;
-      loser = this.player;
+      this.outcome = "Lose";
     }
-    this.calculatePayout(winner, loser);
+    this.calculatePayout();
   }
 
-  calculatePayout(winner, loser) {
-    //need additional logic to determine if any player has a natural and therfore wins 1.5x payout
-    //maybe add hasNatural to player property and set to false at the end of every game, set to true if natural after dealing
-    let payout = winner.currentBetAmount + loser.currentBetAmount;
-    this.displayWinner(winner, loser, payout);
+  calculatePayout() {
+    let payout;
+    if (this.outcome === "Win") {
+      payout = this.player.currentBetAmount * 1.5;
+    } else if (this.outcome === "Lose") {
+      payout = -this.player.currentBetAmount;
+    } else {
+      payout = 0;
+    }
+    this.displayWinner(payout);
   }
 
-  displayWinner(winner, loser, payout) {
-    winner.updateTotalMoney(payout);
-    loser.updateTotalMoney(-loser.currentBetAmount);
-    //something to consider: what happens when computer is the dealer - should the dealer have totalMoney?
-    //if so, what happens when the dealer runs out of money
-    return `${winner.name} wins ${payout}!`;
+  displayWinner(payout) {
+    this.player.updateTotalMoney(payout);
+    if (this.outcome === "Win") {
+      return `Congratulations! You ${this.outcome} $${payout}!`;
+    } else if (this.outcome === "Lose") {
+      return `Sorry! You ${this.outcome} $${-payout} :(`;
+    } else {
+      return `You tied! You get back all your money.`;
+    }
   }
 }
