7a720154ede742f9e3e16d986391082a6f430713
Karen <devkarenc@gmail.com>
2021-07-30 19:59:29 -0400

    Destructure props in the CounterDisplay component

--- client/components/CounterDisplay.js
+++ client/components/CounterDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const CounterDisplay = (label) => (
+export const CounterDisplay = ({ label }) => (
   <div className={`${label}-counter-container`}>
     <div className={`${label}-counter`}>
       <p>{label}</p>
