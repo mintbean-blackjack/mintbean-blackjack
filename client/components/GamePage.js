60f290cda73a51bb649dac64d523f901e858328f
Karen <devkarenc@gmail.com>
2021-07-30 19:14:52 -0400

    Add GamePage component

---
+++ client/components/GamePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const GamePage = props => {
+  return (
+    // nav bar
+    // game table
+  )
+}
---
+++ client/components/GamePageNavBar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
+
+const GamePageNavbar = ({ handleClick, isLoggedIn }) => {
+  const [showRules, setShowRules] = useState(() => false);
+
+  const rulesClickHandler = () => {
+    setShowRules(!showRules);
+  };
+
+  <div>
+    <nav>
+      <div>
+        {/* insert logo */}
+        {/* <Link to="/home">{logo}</Link> */}
+        <Button label="Rules" clickHandler={rulesClickHandler} />
+      </div>
+      {isLoggedIn ? (
+        <div>
+          <a href="#" onClick={handleClick}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      )}
+    </nav>
+    <hr />
+  </div>;
+};
+
+/**
+ * CONTAINER
+ */
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.auth.id,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleClick() {
+      dispatch(logout());
+    },
+  };
+};
+
+export default connect(mapState, mapDispatch)(GamePageNavbar);
