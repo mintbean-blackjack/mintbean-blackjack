47b78c3fcc2b09335da721405869f04a5cefa17f
Fernanda Penfold <fpenfold723@gmail.com>
2021-07-30 09:39:41 -0400

    feat: adds click handlers for game-play buttons

--- client/classes/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Game } from './Game';
-export { default as Dealer } from './Dealer';
-export { default as Player } from './Player';
-export { default as ComuterPlayer } from './ComputerPlayer';
--- client/components/GameTable.js
+++ client/components/GameTable.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react';
 import Game from '../classes/Game';
-import Dealer from '../classes/Dealer';
-import Player from '../classes/Player';
-import ComputerPlayer from '../classes/ComputerPlayer';
 
-export default function GameTable() {
+export const GameTable = () => {
   let game;
   let dealer;
   const [player, setPlayer] = useState({});
   const [computerPlayer, setComputerPlayer] = useState({});
+
+  function startGameHandler() {
+    game = new Game();
+    const { _dealer, _player, _computerPlayer } = game;
+    dealer = _dealer;
+    setPlayer(_player);
+    setComputerPlayer(_computerPlayer);
+  }
+
+  function playAgainHandler() {
+    game.playAgain();
+  }
+
+  function hitHandler() {
+    player.hit();
+  }
+
+  function stayHandler() {
+    player.stay();
+  }
+
+  function placeBetHandler(num) {
+    const err = player.placeBet(num);
+  }
+
+  return();
 }
